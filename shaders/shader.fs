5fae0fb5911cc9c2fd5cb9504a014087437f2ade
Børge Lundsaunet <blund@blund.site>
2021-02-07 13:51:21 +0100

    Legg til avstandsfaktor for lyset!

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -19,11 +19,19 @@ uniform vec3 light_pos;
 
 void main()
 {
+  // Brukes for å la lysstyrken avta ved avstand!
+  // Større 'distance_factor' gir lysere lys.
+  float distance_factor = 1.0;
+  float recip_distance  = distance_factor * 1/distance(light_pos, Frag_pos);
+
+  
+  //
   // Beregn ambient lys
-  float ambient_strength = 0.1;
-   
+  float ambient_strength = 0.5;
   vec3  ambient = ambient_strength * light_color;
 
+  
+  //
   // Beregn diffust lys
   vec3 norm      = normalize(Normal);
   vec3 light_dir = normalize(light_pos - Frag_pos);
@@ -31,9 +39,11 @@ void main()
   float diff     = max(dot(norm, light_dir), 0.0);
   vec3  diffuse  = diff * light_color;
 
+  
+  //
   // Beregn spekulært lys
   float specular_strength = 0.5;
-  int   shininess         = 32;
+  int   shininess         = 16;
 
   vec3 view_dir    = normalize(cam_pos - Frag_pos);
   vec3 reflect_dir = reflect(-light_dir, norm);
@@ -41,8 +51,10 @@ void main()
   vec3  specular = specular_strength * spec * light_color; 
   //specular = vec3(0);
 
-  // Make it shine!!
-  vec3 light = (ambient + diffuse + specular) * object_color;
+  
+  //
+  // Sett sammen lysene!!
+  vec3 light = (ambient + diffuse + specular) * object_color * recip_distance;
   color = vec4(light, 1.0);
 
   //color = texture(texture0, texture_coord);
