049fd6702aacc5840510f32e957b82128118c66a
Børge Lundsaunet <blund@blund.site>
2021-02-15 22:52:18 +0100

    Legg til håndtering av argumenter, valg av kompilator

--- compile.sh
+++ compile.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-CC="clang"
-FILES="game.c glad.c linalg.c helpers.c"
+
+CC="tcc"
+while getopts "c:" arg; do
+    case $arg in
+        c)
+            CC=${OPTARG}
+            [[ ! $CC =~ clang|gcc ]] && CC=tcc
+            ;;
+        *)
+            echo "bad argument!"
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+FILES="game.c glad.c linalg.c helpers.c mu/renderer.c mu/microui.c"
 FLAGS="-g"
 SANITIZE="-fsanitize=address -fsanitize-blacklist=support/blacklist.txt"
 LINKER_FLAGS="-Iinclude -lGL -lglfw -ldl -lm"
 
-rm -f main
+echo "Kompilerer med ${CC}"
 $CC $FILES $FLAGS $LINKER_FLAGS  && ./a.out
